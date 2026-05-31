fc8f89b108764218425b0d6f58ef0f156eaabc2c
CLauchanchisan <claudia_casalone@hotmail.com>
2026-05-31 19:20:23 -0300

    feat(ui): implementar efectos visuales dinamicos en barra de furia

---
+++ Assets/scripts/UI/AddFuryOnDeath.cs
@@ -0,0 +1,42 @@
+using System;
+using UnityEngine;
+using VarelaAloisio.Core;
+using VarelaAloisio.Core.Attributes;
+using Core;
+using HealthSystem.Runtime.Components;
+
+
+public class AddFuryOnDeath : MacacoBehaviour
+{
+    [SerializeField] private float furyToAdd = 0.1f;
+    [AutoMap(How.Service, When.Start)]
+    private IFuryManager _furyManager;
+    private HealthComponentExtended _healthComponent;
+
+    protected override void Start()
+    {
+        base.Start();
+        _healthComponent = GetComponent<HealthComponentExtended>();
+        if (_healthComponent != null)
+        {
+            _healthComponent.onDeath.AddListener(OnDeath);
+        }
+    }
+
+    private void OnDeath()
+    {
+        if (_furyManager != null)
+        {
+            _furyManager.AddFury(furyToAdd);
+        }
+    }
+    protected override void OnDisable()          
+    {
+        base.OnDisable();
+        if (_healthComponent != null)
+        {
+            _healthComponent.onDeath.RemoveListener(OnDeath);
+        }
+    }
+
+}
--- Assets/scripts/UI/FuryLabel.cs
+++ Assets/scripts/UI/FuryLabel.cs
@@ -1,9 +1,12 @@
 using Core;
 using TMPro;
 using UnityEngine;
+using System.Collections;
 using VarelaAloisio.Core;
 using VarelaAloisio.Core.Attributes;
 using UnityEngine.UI;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 namespace UI
 {
@@ -11,11 +14,18 @@ namespace UI
     {
         [AutoMap(How.GetComponent, When.Reset | When.Awake)]
         [SerializeField] private TMP_Text label;
-        //[AutoMap(How.GetComponent, When.Reset | When.Awake)]
         [SerializeField] private Image furyBarImage;
         [SerializeField] private string labelFormat = "Fury: {0}";
         [AutoMap(How.Service, When.Start)]
         private IFuryManager _furyManager;
+        [SerializeField] private Volume _globalVolume;
+        private Vignette _vignette;
+        private LensDistortion _lensDistortion;
+        private ChromaticAberration _aberration;
+        [SerializeField] private float maxVignetteIntensity = 0.8f;
+        [SerializeField] private float maxLensDistortion = -0.7f;
+        [SerializeField] private float maxChromaticAberration = 0.5f;
+
 
         protected override void Start()
         {
@@ -24,12 +34,18 @@ namespace UI
             if (_furyManager is not null)
             {
                 _furyManager.OnFuryUpdated += HandleFuryUpdated;
-                
+
                 if (label)
                     label.SetText(string.Format(labelFormat, _furyManager.Fury));
-                    
+
                 if (furyBarImage)
                     furyBarImage.fillAmount = _furyManager.Fury;
+            }
+            if (_globalVolume)
+            {
+                _globalVolume.profile.TryGet(out _vignette);
+                _globalVolume.profile.TryGet(out _lensDistortion);
+                _globalVolume.profile.TryGet(out _aberration);
             }
         }
 
@@ -43,7 +59,16 @@ namespace UI
             if (label)
                 label.SetText(string.Format(labelFormat, newValue));
             if (furyBarImage)
-            furyBarImage.fillAmount = newValue;
+                furyBarImage.fillAmount = newValue;
+            if (newValue > oldValue)
+            {
+               if (_vignette != null && _lensDistortion != null && _aberration != null)
+                {
+                    _vignette.intensity.value = Mathf.Clamp(maxVignetteIntensity * newValue, 0f, maxVignetteIntensity);
+                    _lensDistortion.intensity.value = Mathf.Clamp(maxLensDistortion * newValue, maxLensDistortion, 0f);
+                    _aberration.intensity.value = Mathf.Clamp(maxChromaticAberration * newValue, 0f, maxChromaticAberration);
+                }
+            }
         }
 
         protected override void OnDisable()
