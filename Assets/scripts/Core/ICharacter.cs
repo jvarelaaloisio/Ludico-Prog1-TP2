a1b9887077619d29bac82556073b076adbdbb5c9
jvarelaaloisio <juampyvarela@gmail.com>
2026-06-06 08:50:13 -0300

    [Combat + Visuals] Added: - Auto pickup - Finished idle animations - Finished walking animations - New cartoon sprites

--- Assets/scripts/Core/ICharacter.cs
+++ Assets/scripts/Core/ICharacter.cs
@@ -10,8 +10,10 @@ namespace Core
         GameObject gameObject { get; }
         Vector2 Direction { get; set; }
         IWeapon CurrentWeapon { get; }
+        bool HasWeapon { get; }
         void PickUp(IWeapon weapon);
         bool TryAttack();
+        bool TryThrowWeapon();
         /// <summary /> Start moving, based on <see cref="Direction"/>
         /// <param name="token">Use to stop movement</param>
         void Move(CancellationToken token);
