f15d4fa8f15ad2c0193b6e440641c5879cbffab1
Juan Pablo Varela <33493616+jvarelaaloisio@users.noreply.github.com>
2026-06-23 22:11:48 -0300

    [Cinematics] Added: Cinematic day 1 polish

---
+++ Assets/scripts/Components/FlickerLight.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using VarelaAloisio.Core;
+
+namespace Components
+{
+    public class FlickerLight : MacacoBehaviour
+    {
+        [SerializeField] private new Light2D light;
+        [SerializeField] private AnimationCurve flickerCurve = AnimationCurve.EaseInOut(0, 0, 1, 1);
+        [SerializeField] private float frequency = 1;
+
+        private void Update()
+        {
+            float duration = flickerCurve.keys[flickerCurve.length - 1].time -  flickerCurve.keys[0].time;
+            light.intensity = flickerCurve.Evaluate(Mathf.Sin(Time.time * frequency) / duration);
+        }
+    }
+}
