f48f10fd8eb359ad44fcfaad34842b72d5f064c0
Juan Pablo Varela <33493616+jvarelaaloisio@users.noreply.github.com>
2026-06-27 06:17:08 -0300

    [Visuals] Improved all menu visuals Added: - Gradient shader - PS5 controls in night 1 - Skip cutscene button

---
+++ Assets/scripts/Components/ImageWithRectUV.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageWithRectUV : Image
+{
+    protected override void OnPopulateMesh(VertexHelper vh)
+    {
+        base.OnPopulateMesh(vh);
+
+        Rect r = rectTransform.rect;
+
+        UIVertex v = new UIVertex();
+
+        for (int i = 0; i < vh.currentVertCount; i++)
+        {
+            vh.PopulateUIVertex(ref v, i);
+
+            float u = Mathf.InverseLerp(r.xMin, r.xMax, v.position.x);
+            float y = Mathf.InverseLerp(r.yMin, r.yMax, v.position.y);
+
+            v.uv1 = new Vector2(u, y);
+
+            vh.SetUIVertex(v, i);
+        }
+    }
+}
